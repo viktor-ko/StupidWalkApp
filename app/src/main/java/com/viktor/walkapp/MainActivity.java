8b1cf26f710865be860faf39ef0bfeed197ccc37
noeigenschaften <36310815+noeigenschaften@users.noreply.github.com>
2024-11-21 18:25:07 +0100

    fixed location permissions issues on the physical device

--- app/src/main/java/com/viktor/walkapp/MainActivity.java
+++ app/src/main/java/com/viktor/walkapp/MainActivity.java
@@ -24,6 +24,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -68,8 +69,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void onWalkButtonClick(View view) {
-
-        //get current location using LocationManager
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if (locationManager != null) {
@@ -78,23 +77,48 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
                 return;
             }
-            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
 
+            // Try getting the last known location from GPS first
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            // If GPS location is unavailable, fall back to Network Provider
+            if (lastKnownLocation == null) {
+                lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+            // If the last known location is not null, set the current location to the latitude and longitude of the location
             if (lastKnownLocation != null) {
                 currentLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
 
                 //generate 3 random points within 1-1.5 km from current location
                 List<LatLng> randomPoints = generateRandomPoints(currentLocation);
-
                 //display 3 walking routes from current location to the generated random points
                 displayWalkingRoutes(currentLocation, randomPoints);
-
                 //move camera position to fit all points
                 zoomToPoints(randomPoints, currentLocation);
             } else {
-                Toast.makeText(this, "Location not available", Toast.LENGTH_SHORT).show();
+                // Actively request updates to get the current location
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                        //Generate random points and display routes
+                        List<LatLng> randomPoints = generateRandomPoints(currentLocation);
+                        displayWalkingRoutes(currentLocation, randomPoints);
+                        zoomToPoints(randomPoints, currentLocation);
+                    }
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) { }
+                    @Override
+                    public void onProviderEnabled(String provider) { }
+                    @Override
+                    public void onProviderDisabled(String provider) { }
+                });
+
+                Toast.makeText(this, "Fetching location, please wait...", Toast.LENGTH_SHORT).show();
             }
+        } else {
+            Toast.makeText(this, "Location Manager is unavailable", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -129,13 +153,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         dbHelper = new DatabaseHelper(this);
 
+        //initialize map
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
 
+        //initialize location manager
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
+        //initialize location listener
         LocationListener locationListener = new LocationListener() {
 
             public void onLocationChanged(Location location) {
@@ -367,15 +393,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    //get the closest address to the random point
+    //get the closest address to the random point using Geocoder
     private void resolveAndInsertAddress(LatLng randomPoint, long rowId) {
+        //check if the permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        //start Geocoder
         Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
         try {
+            //fetch the address for the coordinates
             List<Address> addressList = geocoder.getFromLocation(randomPoint.latitude, randomPoint.longitude, 1);
             if (addressList != null && !addressList.isEmpty()) {
                 Address address = addressList.get(0);
                 StringBuilder addressStringBuilder = new StringBuilder();
-
                 //append each address line to the StringBuilder
                 for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
                     addressStringBuilder.append(address.getAddressLine(i));
@@ -394,4 +428,92 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             dbHelper.updateAddress(rowId, "Error getting address");
         }
     }
+
+
+//    public void onWalkButtonClick(View view) {
+//
+//        //get current location using LocationManager
+//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//
+//        if (locationManager != null) {
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+//                return;
+//            }
+//            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
+//
+//            if (lastKnownLocation != null) {
+//                currentLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+//
+//                //generate 3 random points within 1-1.5 km from current location
+//                List<LatLng> randomPoints = generateRandomPoints(currentLocation);
+//
+//                //display 3 walking routes from current location to the generated random points
+//                displayWalkingRoutes(currentLocation, randomPoints);
+//
+//                //move camera position to fit all points
+//                zoomToPoints(randomPoints, currentLocation);
+//            } else {
+//                Toast.makeText(this, "Location not available", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+
+    // onwalkbuttonclick method doing the following:
+    // 1. Get the current location using the LocationManager.
+    // 2. Check if the location permission is granted. If not, request the permission.
+    // 3. Get the last known location from the GPS provider.
+    // 4. If the GPS location is unavailable, fall back to the Network Provider.
+    // 5. If the last known location is not null, set the current location to the latitude and longitude of the location.
+    // 6. Generate 3 random points within 1-1.5 km from the current location.
+    // 7. Display 3 walking routes from the current location to the generated random points.
+    // 8. Move the camera position to fit all points.
+    // 9. If the Location Manager is unavailable, display a toast message.
+    // 10. If the location permission is not granted, request the permission.
+    // 11. Actively request updates to get the current location.
+    // 12. Set the current location to the latitude and longitude of the location.
+
+//    private void resolveAndInsertAddress(LatLng randomPoint, long rowId) {
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//            return;
+//        }
+//        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+//        try {
+//            Log.d("resolveAndInsertAddress", "Fetching address for coordinates: " + randomPoint.latitude + ", " + randomPoint.longitude);
+//            List<Address> addressList = geocoder.getFromLocation(randomPoint.latitude, randomPoint.longitude, 1);
+//            if (addressList != null && !addressList.isEmpty()) {
+//                Log.d("resolveAndInsertAddress", "Address list size: " + addressList.size());
+//                Address address = addressList.get(0);
+//                StringBuilder addressStringBuilder = new StringBuilder();
+//
+//                // Append each address line to the StringBuilder
+//                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+//                    addressStringBuilder.append(address.getAddressLine(i));
+//                    if (i < address.getMaxAddressLineIndex()) {
+//                        addressStringBuilder.append(", ");
+//                    }
+//                }
+//                // Add address in the database
+//                String addressString = addressStringBuilder.toString();
+//                Log.d("resolveAndInsertAddress", "Resolved address: " + addressString);
+//                dbHelper.updateAddress(rowId, addressString);
+//            } else {
+//                Log.d("resolveAndInsertAddress", "No address found");
+//                dbHelper.updateAddress(rowId, "No address found");
+//            }
+//        } catch (IOException e) {
+//            Log.e("GeocoderError", "Error fetching address: " + e.getMessage());
+//            dbHelper.updateAddress(rowId, "Error fetching address: " + e.getMessage());
+//        } catch (IllegalArgumentException e) {
+//            // Handle invalid latitude or longitude
+//            Log.e("GeocoderError", "Invalid coordinates provided: " + e.getMessage(), e);
+//            dbHelper.updateAddress(rowId, "Invalid coordinates");
+//        }
+//    }
 }
+
